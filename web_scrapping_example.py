07609c94b357e43469b372e72165e74aaa60e277
Bobby K Bose <57045659+bobbykbose37@users.noreply.github.com>
2021-10-31 11:17:49 +0530

    First Commit

--- web_scrapping_example.py
+++ web_scrapping_example.py
@@ -1,5 +1,4 @@
-#Python program to scrape website
-#and save quotes from website
+
 import requests
 from bs4 import BeautifulSoup
 import csv
@@ -9,7 +8,7 @@ r = requests.get(URL)
 
 soup = BeautifulSoup(r.content, 'html5lib')
 
-quotes=[] # a list to store quotes
+quotes=[] 
 
 table = soup.find('div', attrs = {'id':'all_quotes'})
 
